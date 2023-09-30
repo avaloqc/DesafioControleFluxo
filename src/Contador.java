9697f2dcd2613267f755d78cde0b0e1b61667988
avalo-lnx <avalo.rf@gmail.com>
2023-09-30 16:05:39 -0400

    refactor: :recycle: Refactor code to make it similar to the given template

--- src/Contador.java
+++ src/Contador.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Contador {
     public static void main(String[] args) throws ParametrosInvalidosException {
-        Scanner reader = new Scanner(System.in);
-        int numero1 = reader.nextInt();
-        int numero2 = reader.nextInt();
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
 
         try {
-            Imprimir(numero1, numero2);
+            contar(parametroUm, parametroDois);
+
         } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
     }
 
-    public static void Imprimir(int num1, int num2) throws ParametrosInvalidosException {
+    static void contar(int num1, int num2) throws ParametrosInvalidosException {
         if (num2 < num1) throw new ParametrosInvalidosException();
-        for (int i = 1; i <= num2 - num1; i++) {
+        int contagem = num2 - num1;
+        for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimido o número " + i);
         }
     }
